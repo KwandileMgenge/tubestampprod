4499b84b4f14f37bf27abd8cb19fb62310c0b236
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2026-04-14 13:13:55 +0200

    UI refined and looks clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import LandingPage from './unAuth/components/LandingPage'
+import "./App.css";
+import LandingPage from "./unAuth/components/LandingPage";
 
 function App() {
-  return <LandingPage />
+  return (
+    <div className="app-shell">
+      <LandingPage />
+    </div>
+  );
 }
 
-export default App
+export default App;
